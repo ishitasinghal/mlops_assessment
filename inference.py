0a1645d0677a63ca9606b2148d5799a8138bde97
Ishita Singhal <ishitasinghal08@gmail.com>
2024-05-09 15:20:00 +0000

    Merge pull request #2 from ishitasinghal/gradio
    Gradio

--- inference.py
+++ inference.py
@@ -15,4 +15,33 @@ def predict(image):
     return f'Number is: {int(np.argmax(prediction))}'
 
 iface = gr.Interface(fn=predict, inputs="image", outputs="label")
-iface.launch(server_name="0.0.0.0")
+
+
+# fastapi
+import fastapi
+import gradio_client
+from pydantic import BaseModel
+app = fastapi.FastAPI()
+
+class Image(BaseModel):
+    url: str
+
+@app.post("/predict")
+def predict(image: Image):
+    client = gradio_client.Client("http://192.168.1.206:31238/")
+    result = client.predict(
+            image= gradio_client.file(image.url),
+            api_name="/predict"
+    )
+    return result
+    return image.url
+
+def run_uvicorn_app():
+    uvicorn.run(app, host="0.0.0.0", port=5000)
+    print("Server is running on http://0.0.0.0:5000")
+
+if __name__ == "__main__":
+    import uvicorn
+    import threading
+    threading.Thread(target=run_uvicorn_app).start()
+    iface.launch(server_name="0.0.0.0")
--- readme.md
+++ readme.md
@@ -2,5 +2,31 @@
 
 ```bash
 docker build -t mlops .
-docker run --rm -it -p 7860:7860 mlops
+docker run --rm -it -p 7860:7860 -p 5000:5000 mlops
 ```
+
+## How to test
+
+```bash
+wget --no-check-certificate --quiet \
+  --method POST \
+  --timeout=0 \
+  --header 'Content-Type: application/json' \
+  --body-data '{
+    "url": "https://raw.githubusercontent.com/ishitasinghal/mlops_assessment/gradio/digits/8.png"
+}' \
+   'http://localhost:5000/predict'
+```
+
+
+## Screenshots
+
+
+Postman
+![Postman](/docs/postman.png?raw=true "Postman")
+
+Gradio UI
+![Gradio UI](/docs/browser.png?raw=true "Gradio UI")
+
+Kubernetes Resources
+![K3S Deployment](/docs/k3s.png?raw=true "Kubernetes Resources")
